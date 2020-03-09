502709d03d5b488761040d1ed19a5068de91d285
Yazan Ibrahim <yiibrahim@rawabi.ps>
2020-03-09 14:04:27 +0200

    final change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    I LOVE LARAVEL
+                    I LOVE LARAVEL hi
                 </div>
 
                 <div class="links">
