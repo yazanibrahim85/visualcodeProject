78304c3c90bdeaeb52a15884a3d6d31b24659a7e
Yazan Ibrahim <yiibrahim@rawabi.ps>
2020-03-09 13:47:19 +0200

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    I LOVE LARAVEL
                 </div>
 
                 <div class="links">
